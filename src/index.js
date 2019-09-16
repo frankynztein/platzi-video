d61c9bccfc8878ccb3f3ed1647ae370e74b131ac
frankynztein <arellanfranciree@gmail.com>
2019-09-16 17:26:04 -0500

    Componentes y estilos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import App from './containers/App';
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // render recibe dos parámetros: el componente y
 // dónde empujare ese componente
