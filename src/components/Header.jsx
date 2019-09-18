cbadbc4291474ee9207452d018a27155e5de7108
frankynztein <arellanfranciree@gmail.com>
2019-09-17 23:06:44 -0500

    Images working - Webpack

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss';
-// import { logo } from '../assets/static/logo-platzi-video-BW2.png';
-// import userIcon from '../assets/static/user-icon.png';
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import userIcon from '../assets/static/user-icon.png';
 
 const Header = () => (
   <header className='header'>
-    <img className='header__img' src='' alt='Platzi Video Logo' />
+    <img className='header__img' src={logo} alt='Platzi Video Logo' />
     <div className='header__menu'>
       <div className='header__menu--profile'>
-        <img src='' alt='User-Icon' />
+        <img src={userIcon} alt='User-Icon' />
         <p>Perfil</p>
       </div>
       <ul>
